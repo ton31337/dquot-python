92d828d235458fc27cc4684f640955332ef4d613
Donatas Abraitis <donatas.abraitis@gmail.com>
2017-05-19 00:41:23 +0300

    Add more packages

--- setup.py
+++ setup.py
@@ -3,12 +3,17 @@ from setuptools import find_packages
 
 setup(
     name = 'dquota',
-    version = '0.0.5',
+    version = '0.0.6',
     author = 'Donatas Abraitis',
     author_email = 'donatas.abraitis@gmail.com',
     description = 'Handle quota notifications using generic netlink',
     url = 'https://github.com/ton31337/dquot-python',
-    packages = find_packages(),
+    packages = ['dquota',
+                'dquota.providers',
+                'dquota.providers.stdout',
+                'dquota.providers.redis',
+                'dquota.providers.rabbitmq'
+    ],
     install_requires = [
         'pika',
         'redis'
