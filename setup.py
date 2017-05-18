63f96cc65b1577ea745e26f8954acf5e836299e0
Donatas Abraitis <donatas.abraitis@gmail.com>
2017-05-19 00:43:03 +0300

    Add dquota/providers/__init__.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name = 'dquota',
-    version = '0.0.6',
+    version = '0.0.7',
     author = 'Donatas Abraitis',
     author_email = 'donatas.abraitis@gmail.com',
     description = 'Handle quota notifications using generic netlink',
