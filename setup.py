0760cca3daf4eda57f1c7cee651596945eabcef7
Donatas Abraitis <donatas.abraitis@gmail.com>
2017-05-19 00:04:12 +0300

    Add author and author_email to setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ from setuptools import find_packages
 setup(
     name = 'dquota',
     version = '0.0.1',
+    author = 'Donatas Abraitis',
+    author_email = 'donatas.abraitis@gmail.com',
     description = 'Handle quota notifications using generic netlink',
     url = 'https://github.com/ton31337/dquot-python',
     download_url = 'https://github.com/ton31337/dquot-python/tarball/0.0.1',
